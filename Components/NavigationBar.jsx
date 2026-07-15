6bb471b3dbb677348608599220eba9367d70f8c7
vincentcibin-a11y <vincentcibin@gmail.com.com>
2026-07-15 23:30:09 +0530

    Navigation Bar completed

---
+++ Components/NavigationBar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavigationBar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">
+                        EV Charging Station
+                    </Link>
+
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ms-auto">
+
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">
+                                    Add Booking
+                                </Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/view">
+                                    View Bookings
+                                </Link>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavigationBar
